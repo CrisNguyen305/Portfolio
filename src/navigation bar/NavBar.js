8a2f4048c9f7e213a90f8779ae8c3ecf6b40d81b
CrisNguyen305 <vanhuu3005@gmail.com>
2022-04-14 17:46:58 +1000

    css navbar update

--- src/navigation bar/NavBar.js
+++ src/navigation bar/NavBar.js
@@ -21,29 +21,35 @@ function NavBar() {
                     Cris.
                 </Link>
             </div>
-            <div className="header_right">
-                <Link 
-                    activeClass="active"
-                    to={"about"}
-                    spy={true}
-                    smooth={true}
-                    offset={-80}
-                    duration={500}
-                    key={1}
-                >
-                    About
-                </Link>
-                <Link smooth to={"/about"}>Experience</Link>
-                <Link smooth to={"/about"}>Work</Link>
-                <Link smooth to={"/about"}>Contact me</Link>
-                {/* <i class="navigation_bar"></i>
-                <i class="navigation_bar">Experience</i>
-                <i class="navigation_bar">Work</i>
-                <i class="navigation_bar">Contact me</i> */}
+            <ul className="header_right">
+                <li className="nav_items">
+                    <Link 
+                        className='nav_items'
+                        activeClass="active"
+                        to={"about"}
+                        spy={true}
+                        smooth={true}
+                        offset={-80}
+                        duration={500}
+                        key={1}
+                    >
+                        About
+                    </Link>
+                </li>
+                <li className="nav_items">
+                    <Link smooth to={"/about"}>Experience</Link>
+                </li>
+                <li className="nav_items">
+                    <Link smooth to={"/about"}>Work</Link>
+                </li>
+                <li className="nav_items">
+                    <Link smooth to={"/about"}>Contact me</Link>
+                </li>
+
                 <a href={Pdf} target="_blank">
                     <button class="navigation_bar_resume">Resume</button>
                 </a>
-            </div>
+            </ul>
         </div>
     )
 }
